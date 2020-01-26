81d2972bf380f3ae8fd90e47b6506ae8282d5677
SG_Muwa <motherlode.muwa@gmail.com>
2020-01-27 01:34:56 +0300

    Исправлены все ошибки от решателя. Были добавлены в связях текст "Л/И".

--- smic.vue
+++ smic.vue
@@ -1,14 +1,14 @@
 <!-- Tufts VUE 3.3.0 concept-map (smic.vue) 2020-01-27 -->
 <!-- Tufts VUE: http://vue.tufts.edu/ -->
 <!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/home/sgmuwa/Downloads/%d0%9b%d0%b0%d0%b1%d1%8b%20%d0%98%d0%a1%20%d0%9f%d0%9f%d0%a0/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
-<!-- Do Not Remove: Saved date Mon Jan 27 01:13:29 MSK 2020 by sgmuwa on platform Linux 5.3.0-26-generic in JVM 11.0.5+10-post-Ubuntu-0ubuntu1.1 -->
+<!-- Do Not Remove: Saved date Mon Jan 27 01:34:30 MSK 2020 by sgmuwa on platform Linux 5.3.0-26-generic in JVM 11.0.5+10-post-Ubuntu-0ubuntu1.1 -->
 <!-- Do Not Remove: Saving version @(#)VUE: built October 8 2015 at 1658 by tomadm on Linux 2.6.32-504.23.4.el6.x86_64 i386 JVM 1.7.0_21-b11(bits=32) -->
 <?xml version="1.0" encoding="US-ASCII"?>
 <LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xsi:noNamespaceSchemaLocation="none" ID="0" label="smic.vue"
     created="1579985886680" x="0.0" y="0.0" width="1.4E-45"
     height="1.4E-45" strokeWidth="0.0" autoSized="false">
-    <resource referenceCreated="1580076809935" size="21150"
+    <resource referenceCreated="1580078070577" size="18043"
         spec="/home/sgmuwa/Documents/git/Decision-Support-Systems-Design-7-semester/smic.vue"
         type="1" xsi:type="URLResource">
         <title>smic.vue</title>
@@ -202,24 +202,24 @@
         <ID2 xsi:type="node">16</ID2>
     </child>
     <child ID="22"
-        label="&#x418;&#x441;&#x43f;&#x440;&#x430;&#x432;&#x43b;&#x435;&#x43d;&#x43e; &#x432; = &#x418;"
-        layerID="1" created="1579991772568" x="875.6335" y="227.86493"
-        width="252.3039" height="213.66193" strokeWidth="1.0"
+        label="&#x418;&#x441;&#x43f;&#x440;&#x430;&#x432;&#x43b;&#x435;&#x43d;&#x43e; &#x432; = &#x41b;/&#x418;"
+        layerID="1" created="1579991772568" x="875.8761" y="230.28055"
+        width="260.56995" height="211.35019" strokeWidth="1.0"
         autoSized="false" controlCount="1" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
         <textColor>#404040</textColor>
         <font>Arial-plain-11</font>
         <URIString>http://vue.tufts.edu/rdf/resource/dedbd8bd7f00010107fe6c8de0251e6e</URIString>
-        <point1 x="945.616" y="441.02686"/>
-        <point2 x="876.1335" y="229.80176"/>
+        <point1 x="947.4085" y="441.13074"/>
+        <point2 x="876.3761" y="230.78055"/>
         <ID1 xsi:type="node">15</ID1>
         <ID2 xsi:type="node">12</ID2>
-        <ctrlPoint0 x="1240.0" y="210.5" xsi:type="point"/>
+        <ctrlPoint0 x="1243.0" y="232.5" xsi:type="point"/>
     </child>
     <child ID="23"
-        label="&#x421;&#x43a;&#x430;&#x447;&#x435;&#x43d;&#x44b; &#x432; = &#x418;"
-        layerID="1" created="1579991796092" x="844.5021" y="239.36108"
-        width="88.0" height="58.580933" strokeWidth="1.0"
+        label="&#x421;&#x43a;&#x430;&#x447;&#x435;&#x43d;&#x44b; &#x432; = &#x41b;/&#x418;"
+        layerID="1" created="1579991796092" x="838.0021" y="239.36108"
+        width="101.0" height="58.580933" strokeWidth="1.0"
         autoSized="false" controlCount="1" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
         <textColor>#404040</textColor>
@@ -232,9 +232,9 @@
         <ctrlPoint0 x="888.0" y="256.5" xsi:type="point"/>
     </child>
     <child ID="24"
-        label="&#x41f;&#x43e;&#x441;&#x447;&#x438;&#x442;&#x430;&#x43d;&#x43e; = &#x418;"
+        label="&#x41f;&#x43e;&#x441;&#x447;&#x438;&#x442;&#x430;&#x43d;&#x43e; = &#x41b;/&#x418;"
         layerID="1" created="1579991806650" x="875.37506" y="220.6775"
-        width="321.2563" height="274.55652" strokeWidth="1.0"
+        width="327.7563" height="274.55652" strokeWidth="1.0"
         autoSized="false" controlCount="1" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
         <textColor>#404040</textColor>
@@ -262,7 +262,7 @@
         <ctrlPoint0 x="1116.0" y="300.5" xsi:type="point"/>
     </child>
     <child ID="26"
-        label="&#x41e;&#x442;&#x444;&#x438;&#x43b;&#x44c;&#x442;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x43e; = &#x418;"
+        label="&#x41e;&#x442;&#x444;&#x438;&#x43b;&#x44c;&#x442;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x43e; = &#x41b;/&#x418;"
         layerID="1" created="1580075081793" x="625.272" y="499.90567"
         width="283.72467" height="91.69479" strokeWidth="1.0"
         autoSized="false" controlCount="1" arrowState="2" xsi:type="link">
